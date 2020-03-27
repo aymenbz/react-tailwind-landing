debf7b39d972f0d90a0f792d44a8211c7c7c3a3f
Aymenbz <aymenweb5@gmail.com>
2020-03-28 00:25:03 +0100

    Updates

---
+++ precache-manifest.1989a49d9fc96ebfad392d38c9339b4d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2ad8d57eb88d3c37557a189c48629a",
+    "url": "/react-tailwind-landing/index.html"
+  },
+  {
+    "revision": "f27c1663f7feeb35d797",
+    "url": "/react-tailwind-landing/static/css/main.68b93dda.chunk.css"
+  },
+  {
+    "revision": "1c2097c365916dcdd3d8",
+    "url": "/react-tailwind-landing/static/js/2.0385f266.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tailwind-landing/static/js/2.0385f266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f27c1663f7feeb35d797",
+    "url": "/react-tailwind-landing/static/js/main.60ca941e.chunk.js"
+  },
+  {
+    "revision": "8cf5c63059f7a058a623",
+    "url": "/react-tailwind-landing/static/js/runtime-main.0caba225.js"
+  },
+  {
+    "revision": "0711236e1756c5c11176da714fd22ff0",
+    "url": "/react-tailwind-landing/static/media/badge.0711236e.svg"
+  },
+  {
+    "revision": "9aafb857511f19d76dab95ed66506c56",
+    "url": "/react-tailwind-landing/static/media/bg-head.9aafb857.png"
+  },
+  {
+    "revision": "5af5e8d59bab1f4ca5967a7f64f3a0be",
+    "url": "/react-tailwind-landing/static/media/business.5af5e8d5.svg"
+  },
+  {
+    "revision": "074e4e7306cdbf6ff1c1adfac4639aa7",
+    "url": "/react-tailwind-landing/static/media/camera.074e4e73.svg"
+  },
+  {
+    "revision": "763fcb53206f0760e6836db816d59874",
+    "url": "/react-tailwind-landing/static/media/hero-video.763fcb53.png"
+  },
+  {
+    "revision": "00a900c7743334021cdb6a04469fa4b1",
+    "url": "/react-tailwind-landing/static/media/icon-bg.00a900c7.svg"
+  },
+  {
+    "revision": "7423337b70a34f13dea47794f3054e38",
+    "url": "/react-tailwind-landing/static/media/logo.7423337b.svg"
+  },
+  {
+    "revision": "5a7e3d3085677ab66293ba8e123de91d",
+    "url": "/react-tailwind-landing/static/media/payment-card.5a7e3d30.svg"
+  },
+  {
+    "revision": "2de75042ff05baf2abf4c55bc5cece3f",
+    "url": "/react-tailwind-landing/static/media/reload.2de75042.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-tailwind-landing/precache-manifest.1989a49d9fc96ebfad392d38c9339b4d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tailwind-landing/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
